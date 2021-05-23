a827a046f86e13a77e35df49fa0325229687ac00
underneth <davidmduarteoliveira@gmail.com>
2021-05-23 20:38:14 +0100

    Exercício PHP_MySQL_03a

---
+++ originais/p18_avila_bd_03a/public/connections/connection.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+function new_db_connection()
+{
+    // Define working environment
+    //$env = "labmm";
+    $env = "localhost";
+
+    // Variables for the database connection
+    if ($env == "labmm") {
+        $hostname = 'labmm.clients.ua.pt';
+        $username = "deca_19L4_p_web";
+        $password = "1rV0Yyyy";
+        $dbname = "deca_19l4_p";
+    }
+    if ($env == "localhost") {
+        $hostname = 'localhost';
+        $username = "root";
+        $password = "root";
+        $dbname = "projeto_aula";
+    }
+
+// Makes the connection
+    $local_link = mysqli_connect($hostname, $username, $password, $dbname);
+
+// If it fails to connect then die and show errors
+    if (!$local_link) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+// Define charset to avoid special chars errors
+    mysqli_set_charset($local_link, "utf8");
+
+    // Return the link
+    return $local_link;
+}
